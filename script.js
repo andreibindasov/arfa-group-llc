5405c585a8d519f9cf42804c081cbd3b268bede3
Andrei <bindasov.andrei@gmail.com>
2025-04-08 14:46:56 -0600

    ready

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+window.onload = () => {
+    setTimeout(() => {
+        document.querySelector('body').classList.add("display")
+    }, 2100)
+}
+
 document.querySelector('.hamburger-menu').addEventListener('click', ()=>{
     document.querySelector('.container').classList.toggle('change')
 })
@@ -8,3 +14,17 @@ document.querySelector('.scroll-btn').addEventListener('click', ()=>{
         document.querySelector('html').style.scrollBehavior = "unset"
     }, 1000)
 })
+
+const menuItems = document.querySelectorAll('.menu-link');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', (event) => {
+      event.preventDefault();
+      const targetId = item.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+  
+      targetElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
